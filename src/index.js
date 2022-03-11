3bac545a593fd976ad013a3d31b064a91f79b639
WalukaUI <cwaluka@yahoo.com>
2022-03-11 00:02:32 -0800

    styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {});
+//section animation slide in and fadein-------------------
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      document.querySelectorAll(".js-scroll").classList.add("divImage");
+      document.querySelectorAll(".jscontent").classList.add("divContent");
+    }
+  });
+});
+observer.observe(document.querySelectorAll(".section"));
